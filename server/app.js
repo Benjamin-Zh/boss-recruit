73923e38bdf2af256b03d10fe359f164e7907929
zhangfeng <zhangfeng@wifi.com>
2019-07-22 14:09:35 +0800

    feat:  first commit

---
+++ server/app.js
@@ -0,0 +1,44 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const mongoose = require('mongoose');
+
+const DB_URL = 'mongodb://localhost:27017/boss-recruit';
+
+mongoose.connect(DB_URL, { useNewUrlParser: true });
+mongoose.connection.on('connected', () => {
+  console.log('mongodb connected...');
+});
+
+const User = mongoose.model('user', new mongoose.Schema({
+  userName: { type: String, require: true },
+  age: { type: Number, require: true },
+}));
+
+User.create({
+  userName: '',
+  age: 24,
+});
+
+const app = new Koa();
+const router = new Router();
+
+router
+  .get('/', async (ctx, next) => {
+    const body = `<h3>Koa App</h3>`;
+
+    ctx.set('Content-Type', 'text/html');
+    ctx.body = body;
+
+    await next();
+  })
+  .get('/data', async (ctx, next) => {
+    const res = await User.find({});
+
+    ctx.body = res;
+
+    await next();
+  });
+
+app
+  .use(router.routes())
+  .listen(3003, () => console.log('Server is running at 3003...'));
--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function mapStateToProps(state) {
+  return { state };
+}
+
+const actionCreators = {
+  add: () => ({ type: 'ADD' }),
+  minus: () => ({ type: 'MINUS' }),
+};
+
+@connect(mapStateToProps, actionCreators)
+class App {
+  render() {
+    const { props } = this;
+
+    return (
+      <div className="App">
+        {props.state}
+        <button onClick={props.add}>add</button>
+        <button onClick={props.minus}>minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
